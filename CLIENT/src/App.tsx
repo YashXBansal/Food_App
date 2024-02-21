56a31046b66882278edb1649144a56451961e4f5
YashXBansal <yashbansal414@gmail.com>
2024-02-21 16:32:30 +0530

    Initialising the app

---
+++ CLIENT/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='font-serif bg-red-600 underline text-blue-300'>
+      <button>App</button>
+    </div>
+  )
+}
+
+export default App
